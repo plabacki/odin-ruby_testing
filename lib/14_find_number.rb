ab9456f1d21c99b0b6215cd86568856c587287e3
plabacki <plabacki97@gmail.com>
2023-08-29 17:52:50 +0200

    Solved example number 14

--- lib/14_find_number.rb
+++ lib/14_find_number.rb
@@ -11,4 +11,17 @@ class FindNumber
     @answer = answer.value
     @guess = guess
   end
+
+  def make_guess
+    @guess = (min + max) / 2
+  end  
+
+  def game_over?
+    answer == guess
+  end 
+  
+  def update_range
+    guess < answer ? @min = guess + 1 : @max = guess - 1    
+  end  
 end
+
